00ff4d8b3d321d152954785631df2fbb235cc6c7
Martin Schorb <martin.schorb@embl.de>
2020-10-28 14:42:08 +0100

    cluster

--- spark_slurm_N5.sh
+++ spark_slurm_N5.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 #SBATCH --job-name=spark-master      # create a short name for your job
-#SBATCH --time=02:00:00          # total run time limit (HH:MM:SS)
+#SBATCH --time=00:30:00          # total run time limit (HH:MM:SS)
 #SBATCH --mail-type=FAIL,BEGIN,END
 #SBATCH --mail-user=schorb@embl.de
 # #  --- Master resources ---
 #SBATCH --nodes=1
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=8G
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks-per-node=1
 # # --- Worker resources ---
 #SBATCH packjob
 #SBATCH --job-name spark-worker
-#SBATCH --nodes=16
-#SBATCH --mem-per-cpu=6G
-#SBATCH --cpus-per-task=4
+#SBATCH --nodes=10
+#SBATCH --mem-per-cpu=8G
+#SBATCH --cpus-per-task=8
 #SBATCH --ntasks-per-node=1
 
 
@@ -22,6 +22,7 @@
 #module load X11
 
 export DISPLAY=""
+
 export JAVA_HOME=`readlink -m /g/emcf/software/render/deploy/jdk*`
 
 
@@ -42,11 +43,12 @@ export SPARK_MEM=$(( $SLURM_MEM_PER_CPU_PACK_GROUP_1 * $SLURM_CPUS_PER_TASK_PACK
 export SPARK_DAEMON_MEMORY=$SPARK_MEM
 export SPARK_WORKER_MEMORY=$SPARK_MEM
 
-CLASS="org.janelia.render.client.spark.SIFTPointMatchClient"
-JARFILE="/g/emcf/software/render/render-ws-spark-client/target/render-ws-spark-client-2.3.1-SNAPSHOT-standalone.jar"
-RENDERPARAMS="--baseDataUrl http://pc-emcf-16.embl.de:8080/render-ws/v1 --owner test --collection test0_mipmap_2D --pairJson /g/emcf/schorb/render-output/tile_pairs_test0_mipmap_z_442_to_450_dist_1.json"
-PARAMS=`cat /g/emcf/schorb/code/JSON_parameters/SBEMImage/SIFTparams_3D`
 
+
+CLASS="org.janelia.saalfeldlab.hotknife.SparkConvertRenderStackToN5"
+JARFILE="/g/emcf/schorb/code/hot-knife/target/hot-knife-0.0.4-SNAPSHOT.jar"
+RENDERPARAMS="--baseUrl http://pc-emcf-16.embl.de:8080/render-ws/v1 --owner SBEM --stack 3D_results0 --project tests"
+PARAMS="--n5Path /g/emcf/schorb/render-output/test-3D.n5 --n5Dataset /render --tileSize 2048,1536 --min -10784,-210502,442 --size 75044,68142,9 --blockSize 128,128,64"
 
 # start MASTER
 
