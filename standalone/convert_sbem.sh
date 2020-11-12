5f54a4f2bc63d20a91361fa2eff21f5ba1dad33f
Martin Schorb <martin.schorb@embl.de>
2020-11-12 08:52:32 +0100

    console style

--- standalone/convert_sbem.sh
+++ standalone/convert_sbem.sh
@@ -2,4 +2,4 @@
 
 source ../dash/utils/setup_py_environment.sh
 
-python $rendermodules/rendermodules/dataimport/generate_EM_tilespecs_from_SBEMImage.py --input_json $1
+python $rendermodules --input_json $1
