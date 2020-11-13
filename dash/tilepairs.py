4aa8df6e60cca7bcf2fbf44128d4ab8488e0a6fa
Martin Schorb <martin.schorb@embl.de>
2020-11-13 15:55:33 +0100

    structure

--- dash/tilepairs.py
+++ dash/tilepairs.py
@@ -6,3 +6,17 @@ Created on Thu Nov 12 16:15:27 2020
 @author: schorb
 """
 
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input,Output,State
+import params
+
+
+
+
+main=html.Div()
+
+
+# Full page layout:
+    
+page = []
