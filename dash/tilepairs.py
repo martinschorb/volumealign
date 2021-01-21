92ed0b5d0bfb41bdf2f82b9fceb591b009c164bc
Martin Schorb <martin.schorb@embl.de>
2021-01-21 11:08:12 +0100

    refactor tilepairs done

--- dash/tilepairs.py
+++ dash/tilepairs.py
@@ -5,20 +5,19 @@ Created on Thu Nov 12 16:15:27 2020
 
 @author: schorb
 """
-
+import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input,Output,State
 import params
 import json
-import requests
 import os
-import subprocess
 import importlib
 
 
 from app import app
 from utils import launch_jobs, pages
+from utils import helper_functions as hf
 
 from callbacks import runstate,render_selector,substack_sel
 
@@ -32,7 +31,6 @@ storeinit = {}
 store = pages.init_store(storeinit, module)
 
 store.append(dcc.Store(id={'component':'runstep','module':module},data='generate'))
-
 
 
 main=html.Div(id={'component': 'main', 'module': module},children=html.H3("Generate TilePairs for Render stack"))
@@ -66,29 +64,6 @@ page.append(page1)
 
 
 
-
-
-
-# gobutton = html.Div(children=[html.Br(),
-#                               html.Button('Start TilePair generation',id={'component':"go", 'module': module}),
-#                               html.Div(id={'component':'buttondiv', 'module': module}),
-#                               html.Div(id={'component':'directory-popup', 'module': module}),
-#                               html.Br(),
-#                               html.Details([html.Summary('Compute location:'),
-#                                             dcc.RadioItems(
-#                                                 options=[
-#                                                     {'label': 'Cluster (slurm)', 'value': 'slurm'},
-#                                                     {'label': 'locally (this submission node)', 'value': 'standalone'}
-#                                                 ],
-#                                                 value='slurm',
-#                                                 labelStyle={'display': 'inline-block'},
-#                                                 id={'component':'compute_sel', 'module': module}
-#                                                 )],
-#                                   id={'component':'compute', 'module': module}),
-#                               html.Br(),
-#                               html.Div(id={'component':'run_state', 'module': module}, style={'display': 'none'},children='wait')])
-
-
 # # # ===============================================
 
 
@@ -101,7 +76,7 @@ page2 = html.Div(id={'component':'page2', 'module': module},
                                                       id={'component':'pairmode', 'module': module}),
                                html.Div(id={'component':'3Dslices', 'module': module},
                                         children=['range of sections to consider:  ',
-                                                  dcc.Input(id={'component':'input_1', 'module': module},type='number',min=1,max=10,value=0)],
+                                                  dcc.Input(id={'component':'sec_input1', 'module': module},type='number',min=1,max=10,value=0)],
                                                       style={'display':'none'})]
                                ,style={'display':'inline,block'}),
                            html.Br()  
@@ -113,112 +88,137 @@ page.append(pages.substack_sel(module))
 
 
 
-# @app.callback([Output(module+'startsection','max'),
-#                 Output(module+'endsection','min')],
-#               [Input(module+'startsection', 'value'),
-#                 Input(module+'endsection','value'),
-#                 Input(module+'input_1','value')]
-#               )
-# def tilepairs_sectionlimits(start_sec,end_sec,sec_range):
+@app.callback([Output({'component':'startsection','module' : module},'max'),
+                Output({'component':'endsection','module' : module},'min')],
+              [Input({'component':'startsection','module' : module}, 'value'),
+                Input({'component':'endsection','module' : module},'value'),
+                Input({'component':'sec_input1','module' : module},'value')]
+              )
+def tilepairs_sectionlimits(start_sec,end_sec,sec_range):
     
-#     if not sec_range is None and not start_sec is None and not end_sec is None:
-#         return end_sec - sec_range, start_sec + sec_range
-#     else:
-#         return end_sec, start_sec
+    if not sec_range is None and not start_sec is None and not end_sec is None:
+        return end_sec - sec_range, start_sec + sec_range
+    else:
+        return end_sec, start_sec
 
 
 
 
-# # # ===============================================
+# # ===============================================
 
-# @app.callback([Output(module+'3Dslices','style'),
-#                 Output(module+'input_1','value')],
-#               Input(module+'pairmode','value'))
-# def tilepairs_3D_status(pairmode):
+@app.callback([Output({'component':'3Dslices','module' : module},'style'),
+                Output({'component':'sec_input1','module' : module},'value')],
+              Input({'component':'pairmode','module' : module},'value'))
+def tilepairs_3D_status(pairmode):
 
-#     if pairmode == '2D':
-#         style = {'display':'none'}
-#         val = 0
-#     elif pairmode == '3D':
-#         style = {'display':'block'}
-#         val = 1
-#     return style, val
+    if pairmode == '2D':
+        style = {'display':'none'}
+        val = 0
+    elif pairmode == '3D':
+        style = {'display':'block'}
+        val = 1
+    return style, val
 
 
 
-# @app.callback([Output(module+'go', 'disabled'),
-#                 Output(module+'store','data'),
-#                 Output(module+'interval1','interval')
-#                 ],
-#               Input(module+'go', 'n_clicks'),
-#               [State(module+'input_1','value'),
-#                 State(module+'compute_sel','value'),
-#                 State(module+'pairmode','value'),
-#                 State(module+'startsection','value'),
-#                 State(module+'endsection','value'),
-#                 State(module+'store','data')]
-#               )                 
-# def tilepairs_execute_gobutton(click,slicedepth,comp_sel,pairmode,startsection,endsection,storage):   
+
+
+# =============================================
+# Start Button
+
+
+
+gobutton = html.Div(children=[html.Br(),
+                              html.Button('Start TilePair generation',
+                                          id={'component': 'go', 'module': module}),
+                              html.Br(),
+                              pages.compute_loc(module),
+                              html.Br(),
+                              html.Div(id={'component': 'run_state', 'module': module}, style={'display': 'none'},children='wait')])
+
+
+page.append(gobutton)
+
+
+@app.callback([Output({'component': 'go', 'module': module}, 'disabled'),
+               Output({'component': 'store_r_launch', 'module': module},'data'),
+               Output({'component': 'store_render_launch', 'module': module},'data')],
+              Input({'component': 'go', 'module': module}, 'n_clicks'),
+              [State({'component':'sec_input1','module' : module},'value'),
+                State({'component':'compute_sel','module' : module},'value'),
+                State({'component':'pairmode','module' : module},'value'),
+                State({'component':'startsection','module' : module},'value'),
+                State({'component':'endsection','module' : module},'value'),
+                State({'component':'store_owner','module' : module},'data'),
+                State({'component':'store_project','module' : module},'data'),
+                State({'component':'stack_dd','module' : module},'value')]
+              ,prevent_initial_call=True)                 
+def tilepairs_execute_gobutton(click,slicedepth,comp_sel,pairmode,startsection,endsection,owner,project,stack):   
+    if click is None: return dash.no_update
+        
+    # prepare parameters:
+    importlib.reload(params)
+
+    run_params = params.render_json.copy()
+    run_params['render']['owner'] = owner
+    run_params['render']['project'] = project
     
-#     if click>0:
-        
-#         # prepare parameters:
-#         importlib.reload(params)
-    
-#         run_params = params.render_json.copy()
-#         run_params['render']['owner'] = storage['owner']
-#         run_params['render']['project'] = storage['project']
-       
-#         run_params_generate = run_params.copy()
-        
-        
-#         #generate script call...
-        
-#         tilepairdir = params.json_run_dir + '/tilepairs_' + params.run_prefix + '_'+ storage['stack'] + '_' + pairmode
-        
-#         if not os.path.exists(tilepairdir): os.makedirs(tilepairdir)
-        
-#         run_params_generate['output_dir'] = tilepairdir
-        
-#         with open(os.path.join(params.json_template_dir,'tilepairs.json'),'r') as f:
-#                 run_params_generate.update(json.load(f))
-        
-#         run_params_generate['output_json'] = tilepairdir + '/tiles_'+ storage['stack'] + '_' + pairmode
-        
-#         run_params_generate['minZ'] = startsection
-#         run_params_generate['maxZ'] = endsection
-        
-#         run_params_generate['stack'] = storage['stack']
-        
-#         if pairmode == '2D':
-#             run_params_generate['zNeighborDistance'] = 0
-#             run_params_generate['excludeSameLayerNeighbors'] = 'False'
-            
-#         elif pairmode == '3D':
-#             run_params_generate['zNeighborDistance'] = slicedepth
-#             run_params_generate['excludeSameLayerNeighbors'] = 'True'
+    run_params_generate = run_params.copy()
     
     
-#         param_file = params.json_run_dir + '/' + module + params.run_prefix + '_' + pairmode + '.json' 
+    #generate script call...
     
-               
-#         with open(param_file,'w') as f:
-#             json.dump(run_params_generate,f,indent=4)
+    tilepairdir = params.json_run_dir + '/tilepairs_' + params.run_prefix + '_'+ stack + '_' + pairmode
     
-#         log_file = params.render_log_dir + '/' + module + params.run_prefix + '_' + pairmode
-#         err_file = log_file + '.err'
-#         log_file += '.log'
+    if not os.path.exists(tilepairdir): os.makedirs(tilepairdir)
+    
+    run_params_generate['output_dir'] = tilepairdir
+    
+    with open(os.path.join(params.json_template_dir,'tilepairs.json'),'r') as f:
+            run_params_generate.update(json.load(f))
+    
+    run_params_generate['output_json'] = tilepairdir + '/tiles_'+ stack + '_' + pairmode
+    
+    run_params_generate['minZ'] = startsection
+    run_params_generate['maxZ'] = endsection
+    
+    run_params_generate['stack'] = stack
+    
+    if pairmode == '2D':
+        run_params_generate['zNeighborDistance'] = 0
+        run_params_generate['excludeSameLayerNeighbors'] = 'False'
         
-#         tilepairs_generate_p = launch_jobs.run(target=comp_sel,pyscript='$rendermodules/rendermodules/pointmatch/create_tilepairs.py',
-#                             json=param_file,run_args=None,target_args=None,logfile=log_file,errfile=err_file)
-            
-#         params.processes[module.strip('_')].extend(tilepairs_generate_p)
+    elif pairmode == '3D':
+        run_params_generate['zNeighborDistance'] = slicedepth
+        run_params_generate['excludeSameLayerNeighbors'] = 'True'
+
+
+    param_file = params.json_run_dir + '/' + module + '_' + params.run_prefix + '_' + pairmode + '.json' 
+
+           
+    with open(param_file,'w') as f:
+        json.dump(run_params_generate,f,indent=4)
+
+    log_file = params.render_log_dir + '/' + module + '_' + params.run_prefix + '_' + pairmode
+    err_file = log_file + '.err'
+    log_file += '.log'
+    
+    tilepairs_generate_p = launch_jobs.run(target=comp_sel,pyscript='$rendermodules/rendermodules/pointmatch/create_tilepairs.py',
+                        json=param_file,run_args=None,target_args=None,logfile=log_file,errfile=err_file)
         
-#         storage['run_state'] = 'running'
-#         storage['log_file'] = log_file
-#         storage['tilepairdir'] = tilepairdir
-        
-#         return True,storage,params.refresh_interval
+    params.processes[module.strip('_')].extend(tilepairs_generate_p)
+    
+    launch_store=dict()
+    launch_store['logfile'] = log_file
+    launch_store['state'] = 'running'
+    
+    outstore = dict()
+    outstore['owner'] = owner
+    outstore['project'] = project
+    outstore['stack'] = stack
+    outstore['tilepairdir'] = tilepairdir
+
+    return True, launch_store, outstore
 
 
 # =============================================
