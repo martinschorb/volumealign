7db51e861c65c51e6edc43bd31ed70e7c12c368a
Martin Schorb <martin.schorb@embl.de>
2021-12-08 12:46:44 +0100

    local spark launcher

--- launchers/localspark.sh
+++ launchers/localspark.sh
@@ -17,7 +17,7 @@ MASTER_CPU="1"
 TIME="00:10:00"
 WORKER_NODES="1"
 WORKER_CPU="1"
-WORKER_MEMPERCPU="4"
+WORKER_MEMPERCPU="1000"
 EMAIL=`whoami`$emaildomain
 LOGDIR=`pwd`
 LOGFILE=sparkslurm-.out
@@ -119,7 +119,6 @@ cd $LOGDIR
 
 export DISPLAY=""
 export JAVA_HOME=`readlink -m $render_dir/deploy/jdk*`
-export LOGDIR=`pwd`
 
 # CLEAN LOGDIR
 
@@ -127,12 +126,12 @@ rm -f $LOGDIR/worker/*/*.jar
 
 export SPARK_HOME=`./pyvar.sh ../dash/params.py  spark_dir`
 JOB="localspark"
-export MASTER_URL="spark://$(hostname):7077"
-export MASTER_WEB="http://$(hostname):8080"
+export MASTER_URL="local[$WORKER_CPU]" #spark://$(hostname):7077"
+# export MASTER_WEB="http://$(hostname):8080"
 
 CLASS="org.janelia.render.client.spark.SIFTPointMatchClient"
 JARFILE="/g/emcf/software/render/render-ws-spark-client/target/render-ws-spark-client-2.3.1-SNAPSHOT-standalone.jar"
-PARAMS="--baseDataUrl http://pc-emcf-16.embl.de:8080/render-ws/v1 --owner SBEM"
+PARAMS="--baseDataUrl http://render.embl.de:8080/render-ws/v1 --owner SBEM"
 
 # PARSE COMMAND LINE ARGUMENTS
 
@@ -140,20 +139,20 @@ mkdir $LOGDIR/$JOB
 
 # SET UP ENV for the spark run
 
-echo $MASTER_WEB > $LOGDIR/$JOB/master
+# echo $MASTER_WEB > $LOGDIR/$JOB/master
 
 export SPARK_LOG_DIR="$LOGDIR/$JOB/logs"
 export SPARK_WORKER_DIR="$LOGDIR/$JOB/worker"
 export SPARK_LOCAL_DIRS="$TMPDIR/$JOB"
 
 
-export SPARK_WORKER_CORES=$SLURM_CPUS_PER_TASK_HET_GROUP_1
+export SPARK_WORKER_CORES=$WORKER_CPU
 
-export TOTAL_CORES=$(($SPARK_WORKER_CORES * $SLURM_JOB_NUM_NODES_HET_GROUP_1))
+export TOTAL_CORES=$WORKER_CPU #$(($SPARK_WORKER_CORES * $WORKER_NODES))
 
 # export SPARK_DRIVER_MEM=$((4 * 1024))
 
-export SPARK_MEM=$(( $SLURM_MEM_PER_CPU_HET_GROUP_1 * $SLURM_CPUS_PER_TASK_HET_GROUP_1))m
+export SPARK_MEM=$(( $WORKER_MEMPERCPU * $SPARK_WORKER_CORES))m
 export SPARK_DAEMON_MEMORY=$SPARK_MEM
 export SPARK_WORKER_MEMORY=$SPARK_MEM
 
@@ -163,16 +162,16 @@ export SPARK_WORKER_MEMORY=$SPARK_MEM
 
 # start MASTER
 
-$SPARK_HOME/sbin/start-master.sh
+# $SPARK_HOME/sbin/start-master.sh
 
 # sleep a tiny little bit to allow master to start
-sleep 5s
+# sleep 5s
 
 #echo Starting slaves
-$SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER_URL -d $SPARK_WORKER_DIR &
+# $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER_URL -d $SPARK_WORKER_DIR &
 
 # again, sleep a tiny little bit
-sleep 5s
+# sleep 5s
 
 
 sparksubmitcall="$SPARK_HOME/bin/spark-submit --master $MASTER_URL --driver-memory 2g --conf spark.default.parallelism=$TOTAL_CORES --conf spark.executor.cores=$SPARK_WORKER_CORES --executor-memory $SPARK_MEM --class $CLASS $JARFILE $PARAMS"
@@ -180,4 +179,4 @@ sparksubmitcall="$SPARK_HOME/bin/spark-submit --master $MASTER_URL --driver-memo
 echo $sparksubmitcall
 $sparksubmitcall
 
-sleep infinity
+# sleep infinity
