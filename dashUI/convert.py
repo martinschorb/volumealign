52c01a4c4aac85ca9f4f9588d13e6c5d33cc2fbb
Martin Schorb <martin.schorb@embl.de>
2022-01-24 15:40:47 +0100

    start with empty input selector

--- dashUI/convert.py
+++ dashUI/convert.py
@@ -36,7 +36,7 @@ inputmodules = [
 
 main = html.Div(children=[html.H3("Import volume EM datasets - Choose type:", id='conv_head'),
                           dcc.Dropdown(id={'component': 'import_type_dd', 'module': module}, persistence=True,
-                                       options=inputtypes, value='...')
+                                       options=inputtypes, value='')
                           ])
 
 stores = [dcc.Store(id={'component': 'store_render_init', 'module': module}, storage_type='session',data=''),
