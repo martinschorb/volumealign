fc3b797584f48ba373ea3dbf62f772c982e93e9b
Martin Schorb <martin.schorb@embl.de>
2022-06-29 16:21:34 +0200

    add local spark launcher

--- launchers/localspark.sh
+++ launchers/localspark.sh
@@ -1,79 +1,20 @@
 #!/bin/bash
 
-# Launcher for Spark on SLURM
-
-runscript="./slurm-spark_test.sh"
-
-# import Parameters
-
-emaildomain=`./pyvar.sh ../dash/params.py  email`
-render_dir=`./pyvar.sh ../dash/params.py  render_dir`
+# Launcher for local Spark
 
 
-#Default values
-
-MASTER_MEM="1"
-MASTER_CPU="1"
-TIME="00:10:00"
-WORKER_NODES="1"
-WORKER_CPU="1"
-WORKER_MEMPERCPU="1000"
-EMAIL=`whoami`$emaildomain
-LOGDIR=`pwd`
-LOGFILE=sparkslurm-.out
-ERRFILE=sparkslurm-.err
 # PARSE COMMAND LINE ARGUMENTS
 
 while [ "$1" != "" ]; do
-    PARAM=`echo $1 | awk -F= '{print $1}'`
-    VALUE=`echo $1 | awk -F= '{print $2}'`
+    PARAM=$(echo "$1" | awk -F= '{print $1}')
+    VALUE=$(echo "$1" | awk -F= '{print $2}')
     case $PARAM in
-        --runscript)
-            runscript=$VALUE
-            shift
-            ;;
-        --email)
-            EMAIL=$VALUE
-            shift
-            ;;
-        --master_mem)
-            MASTER_MEM=$VALUE
-            shift
-            ;;
-        --master_cpu)
-            MASTER_CPU=$VALUE
-            shift
-            ;;
-        --time)
-            TIME=$VALUE
-            shift
-            ;;
-        --worker_nodes)
-            WORKER_NODES=$VALUE
-            shift
-            ;;
-        --worker_cpu)
+        --cpu)
             WORKER_CPU=$VALUE
             shift
             ;;
-        --worker_mempercpu)
-            WORKER_MEMPERCPU=$VALUE
-            shift
-            ;;
-        --worker_mempercpu)
-            runscript=$VALUE
-            shift
-            ;;
-        --logdir)
-	          LOGDIR=$VALUE
-            shift
-            ;;
-        --logfile)
-            LOGFILE=$VALUE
-            shift
-            ;;
-        --errfile)
-            ERRFILE=$VALUE
+        --mem)
+            SPARK_MEM=$VALUE
             shift
             ;;
         --scriptparams)
@@ -111,72 +52,18 @@ while [ "$1" != "" ]; do
 done
 
 
-
-# Build SLURM submission script
-
-mkdir $LOGDIR
-cd $LOGDIR
-
 export DISPLAY=""
-export JAVA_HOME=`readlink -m $render_dir/deploy/jdk*`
 
-# CLEAN LOGDIR
+if [ -z "$JAVA_HOME" ] ; then
+  export JAVA_HOME=$(readlink -m "$RENDER_DIR"/deploy/*jdk*)
+fi
 
-rm -f $LOGDIR/worker/*/*.jar
-
-export SPARK_HOME=`./pyvar.sh ../dash/params.py  spark_dir`
-JOB="localspark"
-export MASTER_URL="local[$WORKER_CPU]" #spark://$(hostname):7077"
-# export MASTER_WEB="http://$(hostname):8080"
-
-CLASS="org.janelia.render.client.spark.SIFTPointMatchClient"
-JARFILE="/g/emcf/software/render/render-ws-spark-client/target/render-ws-spark-client-2.3.1-SNAPSHOT-standalone.jar"
-PARAMS="--baseDataUrl http://render.embl.de:8080/render-ws/v1 --owner SBEM"
-
-# PARSE COMMAND LINE ARGUMENTS
-
-mkdir $LOGDIR/$JOB
-
-# SET UP ENV for the spark run
-
-# echo $MASTER_WEB > $LOGDIR/$JOB/master
-
-export SPARK_LOG_DIR="$LOGDIR/$JOB/logs"
-export SPARK_WORKER_DIR="$LOGDIR/$JOB/worker"
-export SPARK_LOCAL_DIRS="$TMPDIR/$JOB"
-
-
-export SPARK_WORKER_CORES=$WORKER_CPU
-
-export TOTAL_CORES=$WORKER_CPU #$(($SPARK_WORKER_CORES * $WORKER_NODES))
-
-# export SPARK_DRIVER_MEM=$((4 * 1024))
-
-export SPARK_MEM=$(( $WORKER_MEMPERCPU * $SPARK_WORKER_CORES))m
-export SPARK_DAEMON_MEMORY=$SPARK_MEM
-export SPARK_WORKER_MEMORY=$SPARK_MEM
+export MASTER_URL="local[$WORKER_CPU]"
 
 # MAIN CALLS
 #======================================
 
+sparksubmitcall="$SPARK_HOME/bin/spark-submit --master $MASTER_URL --conf spark.default.parallelism=$WORKER_CPU --conf spark.executor.cores=$WORKER_CPU --executor-memory $SPARK_MEM --class $CLASS $JARFILE $PARAMS"
 
-# start MASTER
-
-# $SPARK_HOME/sbin/start-master.sh
-
-# sleep a tiny little bit to allow master to start
-# sleep 5s
-
-#echo Starting slaves
-# $SPARK_HOME/bin/spark-class org.apache.spark.deploy.worker.Worker $MASTER_URL -d $SPARK_WORKER_DIR &
-
-# again, sleep a tiny little bit
-# sleep 5s
-
-
-sparksubmitcall="$SPARK_HOME/bin/spark-submit --master $MASTER_URL --driver-memory 2g --conf spark.default.parallelism=$TOTAL_CORES --conf spark.executor.cores=$SPARK_WORKER_CORES --executor-memory $SPARK_MEM --class $CLASS $JARFILE $PARAMS"
-
-echo $sparksubmitcall
+echo "$sparksubmitcall"
 $sparksubmitcall
-
-# sleep infinity
